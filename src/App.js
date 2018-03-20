0a19299dc6c08d0a9a41edc3e58c4728beb088be
afopefoluwa <fope.ojo@gmail.com>
2018-03-20 12:03:04 +0100

    added input forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import WeatherFc from './WeatherFc';
+import UserInput from './UserInput';
 
 const App = () => {
     return (
         <div className="App">
             <WeatherFc/>
+            <UserInput/>
+            <UserInput/>
         </div>
         )
 
---
+++ src/UserInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './UserInput.css';
+
+class UserInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="UserInput">
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+export default UserInput;
